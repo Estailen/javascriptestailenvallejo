ccd7c0fd360fa947663354de110840874bc0bd3f
Davidgn123 <judagu03@gmail.com>
2025-08-02 11:02:45 -0500

    subiendo archivos

---
+++ inicio.js
@@ -0,0 +1,15 @@
+var numero = 5;
+console.log("el valor del numero es:" + numero);
+
+let nombre = "Estailen Vallejo";
+var edad = 22;
+var hijos = 2;
+var sueldo = 500000
+
+console.log("datos personales");
+console.log("nombre:" + nombre);
+console.log("edad:" + edad + "años");
+console.log("hijos:" + hijos);
+console.log("sueldo basico: $" + sueldo);
+let masculino 
+console.log("mi genero es masculino");
